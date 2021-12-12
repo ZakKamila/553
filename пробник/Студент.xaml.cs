3a7e70cf94038e3931f8b8afd30c9e15c7823b65
kzakirova959@gmail.com <kzakirova959@gmail.com>
2021-12-12 19:05:05 +0300

    Добавьте файлы проекта.

---
+++ пробник/Студент.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace пробник
+{
+    /// <summary>
+    /// Логика взаимодействия для Студент.xaml
+    /// </summary>
+    public partial class Студент : Page
+    {
+        public Студент()
+        {
+            InitializeComponent();
+        }
+    }
+}
